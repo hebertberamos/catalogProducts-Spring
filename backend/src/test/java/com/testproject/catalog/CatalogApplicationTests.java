988aeb42270df0912a4a0f99339b5b1dbf3aefd1
hebert benigno <hebertramos09@gmail.com>
2023-12-05 09:27:56 -0300

    Backend project created

---
+++ backend/src/test/java/com/testproject/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
